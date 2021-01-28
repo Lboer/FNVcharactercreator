a7d48f6f23a112d31be0d623551d384fe3a34a81
Lboer <porthosmine@gmail.com>
2021-01-28 14:22:54 +0100

    setup for block 2

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -2,13 +2,31 @@
   <div class="top">
     <h1>{{ msg }}</h1>
     <div class="container">
+      <!-- Block 1: SPECIAL stats, name and faction. -->
       <div class="special">
+        <div class="special-box">
+          <label class="text">
+            Name
+          </label>
+          <input class="name" value="Courier">
+        </div>
+        <div class="special-box">
+          <label class="text">
+            Faction
+          </label>
+          <select name="factions" class="name" v-model="selectedFaction">
+            <option v-for="faction in factions" v-bind:key="faction.name" v-bind:value="{ name: faction.name, text: faction.description }">
+              {{ faction.name }}
+            </option>
+          </select>
+        </div>
+        <div class="header"></div>
         <div class="special-box">
           <label class="text">
             Strength
           </label>
           <div class="up" v-on:click="upStrength" id="strUp">+</div>
-          <input id="str" class="score" type="number" max="10" min="1" :value="strength">
+          <input class="score" type="number" max="10" min="1" :value="strength">
           <div class="down" v-on:click="downStrength" id="strDown">-</div>
         </div>
         <div class="special-box">
@@ -16,7 +34,7 @@
             Perception
           </label>
           <div class="up" v-on:click="upPerception">+</div>
-          <input id="per" class="score" type="number" max="10" min="1" :value="perception">
+          <input class="score" type="number" max="10" min="1" :value="perception">
           <div class="down" v-on:click="downPerception">-</div>
         </div>
         <div class="special-box">
@@ -24,7 +42,7 @@
             Endurance
           </label>
           <div class="up" v-on:click="upEndurance">+</div>
-          <input id="end" class="score" type="number" max="10" min="1" :value="endurance">
+          <input class="score" type="number" max="10" min="1" :value="endurance">
           <div class="down" v-on:click="downEndurance">-</div>
         </div>
         <div class="special-box">
@@ -32,7 +50,7 @@
             Charisma
           </label>
           <div class="up" v-on:click="upCharisma">+</div>
-          <input id="cha" class="score" type="number" max="10" min="1" :value="charisma">
+          <input class="score" type="number" max="10" min="1" :value="charisma">
           <div class="down" v-on:click="downCharisma">-</div>
         </div>
         <div class="special-box">
@@ -40,7 +58,7 @@
             Intelligence
           </label>
           <div class="up" v-on:click="upIntelligence">+</div>
-          <input id="int" class="score" type="number" max="10" min="1" :value="intelligence">
+          <input class="score" type="number" max="10" min="1" :value="intelligence">
           <div class="down" v-on:click="downIntelligence">-</div>
         </div>
         <div class="special-box">
@@ -48,7 +66,7 @@
             Agility
           </label>
           <div class="up" v-on:click="upAgility">+</div>
-          <input id="agl" class="score" type="number" max="10" min="1" :value="agility">
+          <input class="score" type="number" max="10" min="1" :value="agility">
           <div class="down" v-on:click="downAgility">-</div>
         </div>
         <div class="special-box">
@@ -56,14 +74,19 @@
             Luck
           </label>
           <div class="up" v-on:click="upLuck">+</div>
-          <input id="lck" class="score" type="number" max="10" min="1" :value="luck">
+          <input class="score" type="number" max="10" min="1" :value="luck">
           <div class="down" v-on:click="downLuck">-</div>
         </div>
         <label class="text">
           Remaining points: 
           <div class="barrier"></div>
-          <input id="rem" class="score" type="number" readonly :value="remainingPoints">
+          <input class="score" type="number" readonly :value="remainingPoints">
         </label>
+        <div class="header"></div>
+        <p class="text" v-if="selectedFaction != null">{{ selectedFaction.text }}</p>
+      </div>
+      <!-- Block 2: Starting perks and description -->
+      <div class="special">
       </div>
     </div>
   </div>
@@ -84,7 +107,14 @@ export default {
         intelligence: 5,
         agility: 5,
         luck: 5,
-        remainingPoints: 5
+        remainingPoints: 5,
+        factions: [
+          {name: "NCR", description: "The NCR emphasizes and strives to support a plethora of old world values, such as democracy, personal liberty, and the rule of law. It also aims to restore general order to the wasteland, the improvement and development of infrastructure and economic systems, and overarching peace between people. Similar to institutions of the old world it seeks to emulate, continued expansion has created challenges with territorial control, loyalty, and corruption that plague the Republic and serve to hinder its goals. The NCR is often criticized by residents of the Mojave wasteland as well as other factions for being hawkish, imperialistic, poorly managed and over-extended in the region, and trying to attempt to emulate old world values that led to nuclear holocaust in the first place."},
+          {name: "The Legion", description:"Caesar's Legion is an imperialistic, ultra-reactionary totalitarian dictatorship based on large scale slavery. Founded in 2247 by Edward Sallow, who then renamed himself Caesar, and Joshua Graham (also known as the Malpais Legate). The Legion uses trappings of the ancient Roman Empire as part of a unifying identity imposed on its tribes but does not recreate any cultural, social, or political institutions of ancient Rome. The Legion itself is simply a slave army built on ruthlessly utilitarian principles, supported by several tributary populations."},
+          {name: "Mr. House", description:"Robert Edwin House is the self-styled president, CEO, and sole proprietor of the New Vegas Strip in the Mojave Wasteland in the year 2281. Prior to the Great War, House was an ever-alluring, reclusive enigma to the world and a veritable celebrity with various tabloids and news articles covering his every move in business and private life. His founding of RobCo Industries propelled him to the top through business savvy using mathematical prediction algorithms; gaining the envy of other roboticists and corporations through sheer success. As such, RobCo technology became ubiquitous across the former United States both in the civilian and military sectors, and shaping the technological progression of it."},
+          {name: "Yes Man", description:"Yes Man was originally a generic securitron robot programmed to work for Mr. House like all the other securitrons found on the Strip. He was recovered by Benny and several of the Chairmen after being damaged by a pulse grenade. Following this incident, Followers of the Apocalypse member Emily Ortal examined the securitron in order to learn Mr. House's secrets in exchange for reprogramming him for Benny's personal use. The reprogramming resulted in Yes Man doing exactly as he was told. Yes Man will help you take down House and rule New Vegas in your stead."}
+        ],
+        selectedFaction: null
     }
   },
   methods:{
@@ -204,6 +234,18 @@ a {
   border: 1px solid #666666;
   width: 33%;
 }
+.name{
+  color: #ffb642;
+  clear: none;
+  width: 40%;
+  background: #4d4336;
+  border: 1px solid #666666 !important;
+  padding: 2px;
+  text-align: center;
+  margin-top: 2px;
+  border-radius: 3px;
+  float: right;
+}
 .score{
   color: #ffb642;
   clear: none;
@@ -240,6 +282,13 @@ a {
   font-size: 20px;
   font-weight: bold;
   cursor: pointer;
+}
+.header{
+    font-size: 12px;
+    font-weight: bold;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border-bottom: 2px solid #ffb642;
 }
 .barrier{
   width: 21px;
